d5de495f6abb345b2bddd27514100ad772bb05df
TheChosenWhan <nick.hermam@gmail.com>
2019-04-15 18:13:27 -0600

    Now generates static Mandelbrot set image on launch

--- Fractal Generator/Application.cu
+++ Fractal Generator/Application.cu
@@ -12,10 +12,142 @@
 #include "shader.h"
 #include <stdio.h>
 
-int WIDTH = 500;
-int HEIGHT = 500;
+typedef struct {
+	double r;       // a fraction between 0 and 1
+	double g;       // a fraction between 0 and 1
+	double b;       // a fraction between 0 and 1
+} rgb;
+
+typedef struct {
+	double h;       // angle in degrees
+	double s;       // a fraction between 0 and 1
+	double v;       // a fraction between 0 and 1
+} hsv;
+
+static hsv   rgb2hsv(rgb in);
+static rgb   hsv2rgb(hsv in);
+
+hsv rgb2hsv(rgb in)
+{
+	hsv         out;
+	double      min, max, delta;
+
+	min = in.r < in.g ? in.r : in.g;
+	min = min < in.b ? min : in.b;
+
+	max = in.r > in.g ? in.r : in.g;
+	max = max > in.b ? max : in.b;
+
+	out.v = max;                                // v
+	delta = max - min;
+	if (delta < 0.00001)
+	{
+		out.s = 0;
+		out.h = 0; // undefined, maybe nan?
+		return out;
+	}
+	if (max > 0.0) { // NOTE: if Max is == 0, this divide would cause a crash
+		out.s = (delta / max);                  // s
+	}
+	else {
+		// if max is 0, then r = g = b = 0              
+		// s = 0, h is undefined
+		out.s = 0.0;
+		out.h = NAN;                            // its now undefined
+		return out;
+	}
+	if (in.r >= max)                           // > is bogus, just keeps compilor happy
+		out.h = (in.g - in.b) / delta;        // between yellow & magenta
+	else
+		if (in.g >= max)
+			out.h = 2.0 + (in.b - in.r) / delta;  // between cyan & yellow
+		else
+			out.h = 4.0 + (in.r - in.g) / delta;  // between magenta & cyan
+
+	out.h *= 60.0;                              // degrees
+
+	if (out.h < 0.0)
+		out.h += 360.0;
+
+	return out;
+}
+
+rgb hsv2rgb(hsv in)
+{
+	double      hh, p, q, t, ff;
+	long        i;
+	rgb         out;
+
+	if (in.s <= 0.0) {       // < is bogus, just shuts up warnings
+		out.r = in.v;
+		out.g = in.v;
+		out.b = in.v;
+		return out;
+	}
+	hh = in.h;
+	if (hh >= 360.0) hh = 0.0;
+	hh /= 60.0;
+	i = (long)hh;
+	ff = hh - i;
+	p = in.v * (1.0 - in.s);
+	q = in.v * (1.0 - (in.s * ff));
+	t = in.v * (1.0 - (in.s * (1.0 - ff)));
+
+	switch (i) {
+	case 0:
+		out.r = in.v;
+		out.g = t;
+		out.b = p;
+		break;
+	case 1:
+		out.r = q;
+		out.g = in.v;
+		out.b = p;
+		break;
+	case 2:
+		out.r = p;
+		out.g = in.v;
+		out.b = t;
+		break;
+
+	case 3:
+		out.r = p;
+		out.g = q;
+		out.b = in.v;
+		break;
+	case 4:
+		out.r = t;
+		out.g = p;
+		out.b = in.v;
+		break;
+	case 5:
+	default:
+		out.r = in.v;
+		out.g = p;
+		out.b = q;
+		break;
+	}
+	return out;
+}
+
+int WIDTH = 2000;
+int HEIGHT = 2000;
+
+double maxIterations = 100;
+
+double min_valx = -2;
+double max_valx = 2;
+
+double min_valy = -2;
+double max_valy = 2;
 
 float *texData;
+
+double map(double x, double in_min, double in_max, double out_min, double out_max)
+{
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 int main() {
 	Display display(WIDTH, HEIGHT, "Fractal Generator");
 
@@ -42,11 +174,41 @@ int main() {
 		texData[i * 3 + 2] = 0.7f;
 	}
 
-	for (int i = 0; i < 1000; i++) {
-		texData[i * 3] = 1.0f;
-		texData[i * 3 + 1] = 0.0f;
-		texData[i * 3 + 2] = 0.0f;
+	for (int x = 0; x < WIDTH; x++) {
+		for (int y = 0; y < HEIGHT; y++) {
+			double a = map(x, 0, WIDTH, min_valx, max_valx);
+			double b = map(y, 0, HEIGHT, min_valy, max_valy);
+
+			double ca = a;
+			double cb = b;
+			double n = 0;
+			while (n < maxIterations) {
+				double aa = a * a - b * b;
+				double bb = 2 * a * b;
+
+				a = aa + ca;
+				b = bb + cb;
+				if (a*a + b*b > 4) {
+					break;
+				}
+				n++;
+			}
+			double col = map(n, 0, maxIterations, 0, 1);
+			col = map(sqrt(col), 0, 1, 0, 360);
+			hsv color1;
+			color1.h = col;
+			color1.s = 1;
+			color1.v = n == maxIterations ? 0 : 1;
+
+			rgb color2;
+			color2 = hsv2rgb(color1);
+			texData[(x + (y*WIDTH)) * 3 + 0] = (float)color2.r;
+			texData[(x + (y*WIDTH)) * 3 + 1] = (float)color2.g;
+			texData[(x + (y*WIDTH)) * 3 + 2] = (float)color2.b;
+		}
 	}
+
+
 
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
@@ -75,9 +237,7 @@ int main() {
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, WIDTH, HEIGHT, 0, GL_RGB, GL_FLOAT, texData);
 
-	
 	while (!display.isClosed()) {
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		display.Update();
 	}
